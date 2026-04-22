aa8f9d1c78baaae224ff85abf6039b06eecc5f92
karthick kannan <karthickkannan@pixelwebsolutions.net>
2026-04-22 10:37:57 +0530

    feat: make API base URL dynamic using env

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/tokenStorage";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 // Attach token automatically from encrypted storage
